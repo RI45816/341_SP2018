33fb71ada07331cfd0cc17899a8023e07c8b70f2
sirius16 <amozu16@gmail.com>
2018-02-15 22:34:16 -0500

    8BZ74

--- Homeworks/hw2/q3.cpp
+++ Homeworks/hw2/q3.cpp
@@ -14,10 +14,10 @@ Node * getListFromUser() {
     Node *head = NULL;
     int userInput;
     do {
-        Node * newNode = new Node();
         cout << "\nEnter a number (0 to stop): ";
         cin >> userInput;
         if (userInput != 0) {
+            Node * newNode = new Node();
             newNode->m_data = userInput;
             if (!head)
                 head = newNode;
@@ -36,14 +36,14 @@ int main() {
 
     Node *head, *curr, *next;
 
-            head = getListFromUser();
-            next = head;
+    head = getListFromUser();
+    next = head;
     while (next != NULL) {
         curr = next;
-                next = curr->m_next;
-                cout << "Next #: " << curr->m_data << endl;
-                delete curr;
+        next = curr->m_next;
+        cout << "Next #: " << curr->m_data << endl;
+        delete curr;
     }
-            delete next;
+    delete next;
     return 0;
 }

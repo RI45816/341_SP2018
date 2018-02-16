48c2516e80309cf47048af1bf3f72b310931033e
sirius16 <amozu16@gmail.com>
2018-02-15 22:21:39 -0500

    HJM20

--- Homeworks/hw2/q2.cpp
+++ Homeworks/hw2/q2.cpp
@@ -7,8 +7,25 @@ int binarySearch(int * myNums, int cnt,int value) {
     
     // pos current position in array
     int pos=cnt/2;
+    if (cnt == 1)
+        if (myNums[0]==value)
+            return 0;
+        else
+            return -1;
     
-    if ()
+    if (myNums[pos] == value)
+        return pos;
+    if (myNums[pos]>value) {
+        int length, newNums[pos];
+        for (length =0;length < pos; length++)
+            newNums[length] = myNums[length];
+        return binarySearch(newNums,length,value);
+    } else {
+        int length = 0, newNums[pos];
+        for (int i=pos;i < cnt; length++,i++)
+            newNums[length] = myNums[i];
+         return ~binarySearch(newNums,length,value) ? pos + binarySearch(newNums,length,value) : -1;
+    }
 }
 //
 
---
+++ Homeworks/hw2/q3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int m_data;
+    Node *m_next;
+};
+
+//
+// INSERT YOUR getListFromUser IMPLEMENTATION HERE
+
+Node * getListFromUser() {
+    Node *head = NULL;
+    int userInput;
+    do {
+        Node * newNode = new Node();
+        cout << "\nEnter a number (0 to stop): ";
+        cin >> userInput;
+        if (userInput != 0) {
+            newNode->m_data = userInput;
+            if (!head)
+                head = newNode;
+            else {
+                Node * curr = head;
+                for (; curr->m_next; curr = curr->m_next);
+                curr->m_next = newNode;
+            }
+        }
+    } while (userInput != 0);
+    return head;
+}
+//
+
+int main() {
+
+    Node *head, *curr, *next;
+
+            head = getListFromUser();
+            next = head;
+    while (next != NULL) {
+        curr = next;
+                next = curr->m_next;
+                cout << "Next #: " << curr->m_data << endl;
+                delete curr;
+    }
+    return 0;
+}
